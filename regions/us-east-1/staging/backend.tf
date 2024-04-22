e59b5942f1d1ac9168064a8573269f4b08c0734f
sudhirbatchu9383 <sudhir.batchu@intuitive.cloud>
2024-04-22 15:16:32 +0530

    cleaning

--- regions/us-east-1/staging/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-states"
-    key            = "${var.environment}/terraform.tfstate"
-    region         = "${var.region}"
-    dynamodb_table = "my-lock-table"
-    encrypt        = true
-  }
-}
